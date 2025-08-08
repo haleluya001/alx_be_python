62f302d477d745cb50200830b86f6db1643ec6b4
Haleluya Tigabe <haleluyatigabe1990@gmail.com>
2025-08-08 19:33:43 +0300

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -25,7 +25,6 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book '{self.title}' created.")
 
     def __del__(self):
         """
